3bacf8bd7f542efa0ee55dd61f1826becd96a9e9
neverdiefire <2624086475@qq.com>
2024-11-05 09:08:39 +0800

    整合了Knife4j

---
+++ library-server/src/main/java/com/firewood/conifg/WebMvcConfiguration.java
@@ -0,0 +1,36 @@
+package com.firewood.conifg;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+@Configuration
+@Slf4j
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    /**
+     * 通过knife4j生成接口文档
+     * @return
+     */
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info()
+                .title("图书管理系统")
+                .version("2.0")
+                .description("图书管理系统相关接口"));
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始设置静态资源映射...");
+
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+}
--- library-server/src/main/java/com/firewood/controller/admin/BookController.java
+++ library-server/src/main/java/com/firewood/controller/admin/BookController.java
@@ -1,14 +1,18 @@
 package com.firewood.controller.admin;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/book")
+@Tag(name = "管理端图图书管理接口")
 public class BookController {
 
 
+    @Operation(summary = "获取图书列表")
     @GetMapping
     public String list(){
 
